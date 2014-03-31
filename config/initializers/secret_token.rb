88cd43be6048ddb0851209d21cd44503d2c0446b
David Adams & vincent rotter <student@epicodus.com>
2014-03-31 10:31:12 -0700

    initial commit, with contact view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wikipages::Application.config.secret_key_base = 'a1266da2014ff2979c79d5b86414a233d6167d6006b66546b80b27e213cfd8b8fa9d47b93adc19dfc95c7cb8f6b579fd7450c54f664b88d76b7d00ebf5d522f1'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Wikipages::Application.routes.draw do
+  match('contacts', {via: :get, to: 'contacts#index'})
+  match('contacts/new', {via: :get, to: 'contacts#new'})
+  match('contacts/:id', {via: :get, to: 'contacts#show'})
+end
